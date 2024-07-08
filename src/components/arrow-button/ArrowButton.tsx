50ea1df3d126fe0239dca0e61436f4a0da7214cc
Arseniy Medyakov <medyakov.senya@bk.ru>
2024-07-08 14:58:38 +0300

    open/close icon menu

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -5,15 +5,25 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export type ArrowButtonProps = {
+	isOpen: boolean;
+	onClick: OnClick;
+};
+
+export const ArrowButton = ({ isOpen, onClick }: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
+			onClick={onClick}
 			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={isOpen ? styles.arrow_open : styles.arrow}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,11 +2,19 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setOpen] = useState(false);
+
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setOpen(!isOpen);
+				}}
+			/>
 			<aside className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
